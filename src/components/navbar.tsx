8538110033b4af3fd6f902ecf01258fa802af7b3
Romeu Maleiane <romeumaleiane1@gmail.com>
2025-10-06 15:46:57 +0200

    first commit

---
+++ src/components/navbar.tsx
@@ -0,0 +1,57 @@
+'use client'
+import Image from 'next/image'
+import { Button } from './ui/button'
+import { useGSAP } from "@gsap/react";  
+import gsap from 'gsap'
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+
+gsap.registerPlugin(ScrollTrigger);
+
+
+function Navbar() {
+
+  useGSAP(() => {
+    const navTween = gsap.timeline({
+      scrollTrigger: {
+        trigger: '#navbar',
+        start: 'bottom top',
+      }
+    }) 
+
+    navTween.fromTo('#navbar', { backgroundColor: 'transparent' }, {
+      backgroundColor: '#dddddd50',
+      backgroundFilter: 'blur(10px)',
+      borderRadius: '50px',
+      maxWidth: '70rem', 
+      duration: 1,
+      ease: 'power1.inOut'
+    })
+  },[])
+
+  return (
+    <header id='navbar' className='personalized-conteiner mt-2 py-3  px-7 fixed'>
+      <nav className='flex items-center justify-between h-13'>
+        <div className='gap-3'>
+          <Image src={'/zion-seo-logo.webp'} height={40} width={40} alt='logo' className='rounded-md' />
+        </div>
+
+        <div className=''>
+          <ul className='flex-center text-black gap-5 text-xl font-medium '>
+            <li className='hover:text-black/75'>About</li>
+            <li className='hover:text-black/75'>Features</li>
+            <li className='hover:text-black/75'>Pricing</li>
+          </ul>
+        </div>
+
+        <div className=''>
+          <Button className='px-5 text-lg bg-white hover:bg-white/75 rounded-full'>
+            <span className='bg-gradient-to-r from-green-500 to-blue-400 bg-clip-text text-transparent font-medium'>Try for free</span>
+          </Button>
+        </div>
+      </nav>
+    </header>
+  )
+}
+
+export default Navbar
