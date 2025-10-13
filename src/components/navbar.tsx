694367e38a2e5bd70956ae75f2a499e33c43d2af
Romeu Maleiane <romeumaleiane1@gmail.com>
2025-10-13 23:59:31 +0200

    implement resposivity on navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -6,12 +6,15 @@ import gsap from 'gsap'
 import { ScrollTrigger, SplitText } from "gsap/all";
 import ColoredText from './coloredText';
 import Link from 'next/link';
+import { Menu } from 'lucide-react';
+import { useState } from 'react';
 
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 
 function Navbar() {
+  const [openDropdown, setOpenDropdown] = useState(false)
 
   useGSAP(() => {
     const navTween = gsap.timeline({
@@ -22,37 +25,53 @@ function Navbar() {
     })
 
     navTween.fromTo('#navbar', { backgroundColor: 'transparent' }, {
-      backgroundColor: '#dddddd70',
-      backgroundFilter: 'blur(10px)',
-      borderRadius: '50px',
-      width: '70rem',
+      backgroundColor: '#ffffff70',
+      backdropFilter: 'blur(10px)',
+      borderRadius: '25px',
+      width: '70%',
       duration: 1,
-      ease: 'power1.inOut'
+      ease: 'power1.inOut',
     })
   }, [])
 
   return (
     <header className='font-poppins flex-x-center'>
       <div id='navbar' className='personalized-conteiner mt-2 py-1.5  px-4 fixed z-50'>
-        <nav className='flex items-center justify-between h-13'>
+        <nav className='flex items-center flex-wrap justify-between h-13'>
           <Link href={'/'}>
             <Image src={'/zion-seo-logo.webp'} height={40} width={40} alt='logo' className='rounded-md' />
           </Link>
 
-          <div className=''>
-            <ul className='flex-center text-black gap-5 text-xl font-medium '>
-              <li className='hover:text-black/75'>About</li>
-              <li className='hover:text-black/75'>Features</li>
-              <li className='hover:text-black/75'>Pricing</li>
+          <div className='hidden md:block'>
+            <ul className='flex-center text-black gap-5 md:text-xl text-lg font-medium '>
+              <li className='hover:text-black/75 cursor-pointer'><Link href={'#about'}> About </Link></li>
+              <li className='hover:text-black/75 cursor-pointer'><Link href={'#features'}> Features </Link></li>
+              <li className='hover:text-black/75 cursor-pointer'><Link href={'#pricing'}> Pricing </Link></li>
             </ul>
           </div>
 
-          <div className=''>
-            <Button variant={'primary'}>
+          <div className='hidden md:block'>
+            <Button className='md:text-xl text-lg' variant={'primary'}>
               <ColoredText text='Try for free' />
             </Button>
           </div>
+
+          <button className='md:hidden' onClick={() => setOpenDropdown(!openDropdown)}>
+            <Menu />
+          </button>
         </nav>
+        <div className={`${!openDropdown && 'hidden'} md:hidden space-y-2 mt-2 w-full `}>
+          <ul className='flex flex-col items-start gap-2 text-black text-lg font-medium px-2'>
+            <li className='hover:text-black/75 cursor-pointer'><Link href={'#about'}> About </Link></li>
+            <li className='hover:text-black/75 cursor-pointer'><Link href={'#features'}> Features </Link></li>
+            <li className='hover:text-black/75 cursor-pointer'><Link href={'#pricing'}> Pricing </Link></li>
+          </ul>
+          <div className='px-2'>
+            <Button className='text-lg w-full' variant={'primary'}>
+              <ColoredText text='Try for free' />
+            </Button>
+          </div>
+        </div>
       </div>
     </header>
   )
