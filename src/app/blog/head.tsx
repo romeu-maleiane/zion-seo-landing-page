985f71c91fbf21b3c5639b0f8fd16d82dcefd1bb
Romeu Maleiane <romeumaleiane1@gmail.com>
2026-07-18 18:00:30 +0200

    fix minor issues

--- src/app/blog/head.tsx
+++
@@ -1,3 +0,0 @@
-export default function Head() {
-  return <link rel="stylesheet" href="/blog-article-polish.css" />
-}
--- src/app/blog/template.tsx
+++
@@ -1,3 +0,0 @@
-export default function BlogTemplate({ children }: Readonly<{ children: React.ReactNode }>) {
-  return <>{children}<link rel="stylesheet" href="/blog-article-spacing.css" /></>
-}
